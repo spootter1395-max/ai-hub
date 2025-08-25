e16d5f2b25472c7de9a5d5b04133c0e21ae59bfc
spootter1395-max <spootter1395@gmail.com>
2025-08-25 03:55:12 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker activating.');
+  console.log('✅ Service Worker نصب شد');
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
