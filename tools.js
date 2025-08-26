d03dadf309289f4f6fdd172a0b0923d4b7072830
spootter1395-max <spootter1395@gmail.com>
2025-08-27 01:30:17 +0330

    Create tools.js

---
+++ tools.js
@@ -0,0 +1,11 @@
+function loadTools() {
+  const content = document.getElementById('content');
+  content.innerHTML = `
+    <h2>ابزارهای هوش مصنوعی</h2>
+    <ul>
+      <li>تبدیل متن به گفتار</li>
+      <li>تشخیص تصویر</li>
+      <li>ترجمه هوشمند</li>
+    </ul>
+  `;
+}
